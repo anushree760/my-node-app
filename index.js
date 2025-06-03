7361fab4c47fce23d429cc35e03133696ac2a074
anushree760 <anushree7381@gmail.com>
2025-06-03 01:54:21 -0700

    Your custom commit message here

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
         </style>
       </head>
       <body>
-        <h1>Hello from 👋</h1>
+        <h1>Hello from render👋</h1>
         <p>This is a styled Node.js + Express app deployed with CI/CD on Render.</p>
         <footer>© Project</footer>
       </body>
